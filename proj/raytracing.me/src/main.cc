774ca7500f5f9bb88901ee67cf2f41c24ac1cc12
yueyi <1481796592@qq.com>
2023-04-24 18:16:50 +0800

    update

--- proj/raytracing.me/src/main.cc
+++ proj/raytracing.me/src/main.cc
@@ -1,6 +1,12 @@
 #include <iostream>
 
-int main() {
+int main(int argc, char** argv)
+ {
+    // output argvs
+    std::cout << "got " << argc << " arguments:" << std::endl;
+    for (int i = 0; i < argc; ++i) {
+        std::cout << argv[i] << std::endl;
+    }
 
     // Image
 
