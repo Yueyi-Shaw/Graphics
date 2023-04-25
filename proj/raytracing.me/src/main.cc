c2fe2fadd3f23b899c184bb6a8a2ad5edc928c7e
yueyi <1481796592@qq.com>
2023-04-25 23:11:32 +0800

    img3

--- proj/raytracing.me/src/main.cc
+++ proj/raytracing.me/src/main.cc
@@ -3,6 +3,25 @@
 #include "vec3.h"
 #include <iostream>
 
+bool pure_sphere(const point3 &center, double radius, const ray &r)
+{
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
+color ray_pure_sphere_color(const ray &r)
+{
+    if (pure_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
 color ray_color(const ray &r)
 {
     // middium part is "lighter" than two ends because y here is normalized with x
@@ -24,6 +43,7 @@ int main(int argc, char **argv)
             std::cerr << "parse lesson number: \n \
 1 : Image 1: First PPM image \n \
 2 : Image 2: A blue-to-white gradient depending on ray Y coordinate \n \
+3 : Image 3: Image 3: A simple red sphere \n \
 .ppm will be generate in ./img directory."
                       << std::endl;
         }
@@ -86,6 +106,80 @@ int main(int argc, char **argv)
                 auto v = double(j) / (image_height - 1);
                 ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
                 color pixel_color = ray_color(r);
+                write_color(std::cout, pixel_color);
+            }
+        }
+        break;
+    }
+    case 3:
+    {
+        // Image
+        const auto aspect_ratio = 16.0 / 9.0;
+        const int image_width = 400;
+        const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+        // Camera
+
+        auto viewport_height = 2.0;
+        auto viewport_width = aspect_ratio * viewport_height;
+        auto focal_length = 1.0;
+
+        auto origin = point3(0, 0, 0);
+        auto horizontal = vec3(viewport_width, 0, 0);
+        auto vertical = vec3(0, viewport_height, 0);
+        auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+        // Render
+
+        std::cout << "P3\n"
+                  << image_width << " " << image_height << "\n255\n";
+
+        for (int j = image_height - 1; j >= 0; --j)
+        {
+            std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            for (int i = 0; i < image_width; ++i)
+            {
+                auto u = double(i) / (image_width - 1);
+                auto v = double(j) / (image_height - 1);
+                ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+                color pixel_color = ray_pure_sphere_color(r);
+                write_color(std::cout, pixel_color);
+            }
+        }
+        break;
+    }
+    case 4:
+    {
+        // Image
+        const auto aspect_ratio = 16.0 / 9.0;
+        const int image_width = 400;
+        const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+        // Camera
+
+        auto viewport_height = 2.0;
+        auto viewport_width = aspect_ratio * viewport_height;
+        auto focal_length = 1.0;
+
+        auto origin = point3(0, 0, 0);
+        auto horizontal = vec3(viewport_width, 0, 0);
+        auto vertical = vec3(0, viewport_height, 0);
+        auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+        // Render
+
+        std::cout << "P3\n"
+                  << image_width << " " << image_height << "\n255\n";
+
+        for (int j = image_height - 1; j >= 0; --j)
+        {
+            std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            for (int i = 0; i < image_width; ++i)
+            {
+                auto u = double(i) / (image_width - 1);
+                auto v = double(j) / (image_height - 1);
+                ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+                color pixel_color = ray_pure_sphere_color(r);
                 write_color(std::cout, pixel_color);
             }
         }
