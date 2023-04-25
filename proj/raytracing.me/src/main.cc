9c421279caa7252613b7fec17d1863f8c4ec5fef
yueyi <1481796592@qq.com>
2023-04-25 11:21:01 +0800

    edit make

--- proj/raytracing.me/src/main.cc
+++ proj/raytracing.me/src/main.cc
@@ -3,9 +3,9 @@
 int main(int argc, char** argv)
  {
     // output argvs
-    std::cout << "got " << argc << " arguments:" << std::endl;
+    std::cerr << "got " << argc << " arguments:" << std::endl;
     for (int i = 0; i < argc; ++i) {
-        std::cout << argv[i] << std::endl;
+        std::cerr << argv[i] << std::endl;
     }
 
     // Image
@@ -14,7 +14,7 @@ int main(int argc, char** argv)
     const int image_height = 256;
 
     // Render
-
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
@@ -26,7 +26,7 @@ int main(int argc, char** argv)
             int ig = static_cast<int>(255.999 * g);
             int ib = static_cast<int>(255.999 * b);
 
-            //std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
     std::cerr << "\nDone.\n";
