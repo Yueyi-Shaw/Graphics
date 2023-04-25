80828dc424bb0b7d1f25c0892b5ac655f1b6b6a1
yueyi <1481796592@qq.com>
2023-04-25 14:58:02 +0800

    update

--- proj/raytracing.me/src/main.cc
+++ proj/raytracing.me/src/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "vec3.h"
+#include "color.h"
 
 int main(int argc, char** argv)
  {
@@ -9,7 +10,7 @@ int main(int argc, char** argv)
         std::cerr << argv[i] << std::endl;
     }
     color vector(0,1,0);
-    std::cerr << "vector " << vector[1] << std::endl;
+    std::cerr << "vector " << vector[0] << vector[1] << vector[2] << std::endl;
     // Image
 
     const int image_width = 256;
@@ -20,15 +21,8 @@ int main(int argc, char** argv)
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+            write_color(std::cout, pixel_color);
         }
     }
     std::cerr << "\nDone.\n";
