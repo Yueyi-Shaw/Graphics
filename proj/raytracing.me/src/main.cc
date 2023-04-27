02438463f9c699191c081d3082c587240974aed1
yueyi <1481796592@qq.com>
2023-04-28 01:42:26 +0800

    update 5

--- proj/raytracing.me/src/main.cc
+++ proj/raytracing.me/src/main.cc
@@ -1,6 +1,7 @@
+#include "rtweekend.h"
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "hittable_list.h"
+#include "sphere.h"
 #include <iostream>
 
 bool pure_sphere(const point3 &center, double radius, const ray &r)
@@ -22,33 +23,51 @@ color ray_pure_sphere_color(const ray &r)
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-double normal_sphere(const point3& center, double radius, const ray& r) {
+double normal_sphere(const point3 &center, double radius, const ray &r)
+{
     vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
     auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    if (discriminant < 0) {
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    if (discriminant < 0)
+    {
         return -1.0;
-    } else {
-        return (-b - sqrt(discriminant) ) / (2.0*a);
+    }
+    else
+    {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
     }
 }
 
-color ray_normal_sphere_color(const ray& r) {
-    auto t = normal_sphere(point3(0,0,-1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
-        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+color ray_normal_sphere_color(const ray &r)
+{
+    auto t = normal_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0)
+    {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+    t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 color ray_color(const ray &r)
 {
     // middium part is "lighter" than two ends because y here is normalized with x
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+color ray_color_5(const ray &r, const hittable &world)
+{
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec))
+    {
+        return 0.5 * (rec.normal + color(1, 1, 1));
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
@@ -69,6 +88,7 @@ int main(int argc, char **argv)
 2 : Image 2: A blue-to-white gradient depending on ray Y coordinate \n \
 3 : Image 3: Image 3: A simple red sphere \n \
 4 : Image 4: A sphere colored according to its normal vectors \n \
+5 : Image 5: Resulting render of normals-colored sphere with ground \n \
 .ppm will be generate in ./img directory."
                       << std::endl;
         }
@@ -209,6 +229,48 @@ int main(int argc, char **argv)
             }
         }
         break;
+    }
+    case 5:
+    {
+        // Image
+
+        const auto aspect_ratio = 16.0 / 9.0;
+        const int image_width = 400;
+        const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+        // World
+        hittable_list world;
+        world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+        world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+        // Camera
+
+        auto viewport_height = 2.0;
+        auto viewport_width = aspect_ratio * viewport_height;
+        auto focal_length = 1.0;
+
+        auto origin = point3(0, 0, 0);
+        auto horizontal = vec3(viewport_width, 0, 0);
+        auto vertical = vec3(0, viewport_height, 0);
+        auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+        // Render
+
+        std::cout << "P3\n"
+                  << image_width << ' ' << image_height << "\n255\n";
+
+        for (int j = image_height - 1; j >= 0; --j)
+        {
+            std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            for (int i = 0; i < image_width; ++i)
+            {
+                auto u = double(i) / (image_width - 1);
+                auto v = double(j) / (image_height - 1);
+                ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+                color pixel_color = ray_color_5(r, world);
+                write_color(std::cout, pixel_color);
+            }
+        }
     }
     }
 
