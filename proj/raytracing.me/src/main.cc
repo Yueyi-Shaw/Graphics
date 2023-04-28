15e35f4daba483031ad5bc1b88d3f1556b19e3c7
yueyi <1481796592@qq.com>
2023-04-28 20:00:14 +0800

    lesson 8

--- proj/raytracing.me/src/main.cc
+++ proj/raytracing.me/src/main.cc
@@ -74,6 +74,23 @@ color ray_color_5(const ray &r, const hittable &world)
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
+color ray_color_7(const ray &r, const hittable &world, int depth)
+{
+    hit_record rec;
+
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return color(0, 0, 0);
+    if (world.hit(r, 0, infinity, rec))
+    {
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_color_7(ray(rec.p, target - rec.p), world, depth - 1);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
 int main(int argc, char **argv)
 {
     // handle argvs
@@ -91,6 +108,7 @@ int main(int argc, char **argv)
 4 : Image 4: A sphere colored according to its normal vectors \n \
 5 : Image 5: Resulting render of normals-colored sphere with ground \n \
 6 : Image 6: Before and after antialiasing \n \
+7 : Image 7: First render of a diffuse sphere \n \
 .ppm will be generate in ./img directory."
                       << std::endl;
         }
@@ -273,6 +291,7 @@ int main(int argc, char **argv)
                 write_color(std::cout, pixel_color);
             }
         }
+        break;
     }
     case 6:
     {
@@ -312,6 +331,89 @@ int main(int argc, char **argv)
                 write_color(std::cout, pixel_color, samples_per_pixel);
             }
         }
+        break;
+    }
+    case 7:
+    {
+        // Image
+
+        const auto aspect_ratio = 16.0 / 9.0;
+        const int image_width = 400;
+        const int image_height = static_cast<int>(image_width / aspect_ratio);
+        const int samples_per_pixel = 100;
+        const int max_depth = 50;
+
+        // World
+        hittable_list world;
+        world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+        world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+        // Camera
+        camera cam;
+
+        // Render
+
+        std::cout << "P3\n"
+                  << image_width << ' ' << image_height << "\n255\n";
+
+        for (int j = image_height - 1; j >= 0; --j)
+        {
+            std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            for (int i = 0; i < image_width; ++i)
+            {
+                color pixel_color(0, 0, 0);
+                for (int s = 0; s < samples_per_pixel; ++s)
+                {
+                    auto u = (i + random_double()) / (image_width - 1);
+                    auto v = (j + random_double()) / (image_height - 1);
+                    ray r = cam.get_ray(u, v);
+                    pixel_color += ray_color_7(r, world, max_depth);
+                }
+                write_color(std::cout, pixel_color, samples_per_pixel);
+            }
+        }
+        break;
+    }
+    case 8:
+    {
+        // Image
+
+        const auto aspect_ratio = 16.0 / 9.0;
+        const int image_width = 400;
+        const int image_height = static_cast<int>(image_width / aspect_ratio);
+        const int samples_per_pixel = 100;
+        const int max_depth = 50;
+
+        // World
+        hittable_list world;
+        world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+        world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+        // Camera
+        camera cam;
+
+        // Render
+
+        std::cout << "P3\n"
+                  << image_width << ' ' << image_height << "\n255\n";
+
+        for (int j = image_height - 1; j >= 0; --j)
+        {
+            std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            for (int i = 0; i < image_width; ++i)
+            {
+                color pixel_color(0, 0, 0);
+                for (int s = 0; s < samples_per_pixel; ++s)
+                {
+                    auto u = (i + random_double()) / (image_width - 1);
+                    auto v = (j + random_double()) / (image_height - 1);
+                    ray r = cam.get_ray(u, v);
+                    pixel_color += ray_color_7(r, world, max_depth);
+                }
+                write_color_8(std::cout, pixel_color, samples_per_pixel);
+            }
+        }
+        break;
     }
     }
 
--- proj/raytracing.me/src/sphere.h
+++ proj/raytracing.me/src/sphere.h
@@ -32,10 +32,10 @@ bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &rec) cons
 
     // Find the nearest root that lies in the acceptable range.
     auto root = (-half_b - sqrtd) / a;
-    if (root < t_min || t_max < root)
+    if (root <= t_min || t_max <= root)
     {
         root = (-half_b + sqrtd) / a;
-        if (root < t_min || t_max < root)
+        if (root <= t_min || t_max <= root)
             return false;
     }
 
