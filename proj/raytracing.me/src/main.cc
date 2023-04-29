6c23f3e27741022b00b087e7ec69c7706d9441d9
yueyi <1481796592@qq.com>
2023-04-29 23:05:34 +0800

    update 10 and 11

--- proj/raytracing.me/src/main.cc
+++ proj/raytracing.me/src/main.cc
@@ -2,8 +2,9 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
-#include <iostream>
 #include "camera.h"
+#include "material.h"
+#include <iostream>
 
 bool pure_sphere(const point3 &center, double radius, const ray &r)
 {
@@ -108,6 +109,45 @@ color ray_color_9(const ray &r, const hittable &world, int depth)
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
+color ray_color_10(const ray &r, const hittable &world, int depth)
+{
+    hit_record rec;
+
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return color(0, 0, 0);
+    if (world.hit(r, 0, infinity, rec))
+    {
+        point3 target = rec.p + random_in_hemisphere(rec.normal);
+        return 0.5 * ray_color_10(ray(rec.p, target - rec.p), world, depth - 1);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+color ray_color_11(const ray &r, const hittable &world, int depth)
+{
+    hit_record rec;
+
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return color(0, 0, 0);
+
+    if (world.hit(r, 0.001, infinity, rec))
+    {
+        ray scattered;
+        color attenuation;
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+            return attenuation * ray_color_11(scattered, world, depth - 1);
+        return color(0, 0, 0);
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
 int main(int argc, char **argv)
 {
     // handle argvs
@@ -128,6 +168,8 @@ int main(int argc, char **argv)
 7 : Image 7: First render of a diffuse sphere \n \
 8 : Image 8: Diffuse sphere, with gamma correction \n \
 9 : Image 9: Correct rendering of Lambertian spheres \n \
+10 : Image 10: Rendering of diffuse spheres with hemispherical scattering \n \
+11 : Image 11: Shiny metal \n \
 .ppm will be generate in ./img directory."
                       << std::endl;
         }
@@ -469,6 +511,95 @@ int main(int argc, char **argv)
                     auto v = (j + random_double()) / (image_height - 1);
                     ray r = cam.get_ray(u, v);
                     pixel_color += ray_color_9(r, world, max_depth);
+                }
+                write_color_8(std::cout, pixel_color, samples_per_pixel);
+            }
+        }
+        break;
+    }
+    case 10:
+    {
+        // Image
+
+        const auto aspect_ratio = 16.0 / 9.0;
+        const int image_width = 400;
+        const int image_height = static_cast<int>(image_width / aspect_ratio);
+        const int samples_per_pixel = 100;
+        const int max_depth = 50;
+
+        // World
+        hittable_list world;
+        world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+        world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+        // Camera
+        camera cam;
+
+        // Render
+
+        std::cout << "P3\n"
+                  << image_width << ' ' << image_height << "\n255\n";
+
+        for (int j = image_height - 1; j >= 0; --j)
+        {
+            std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            for (int i = 0; i < image_width; ++i)
+            {
+                color pixel_color(0, 0, 0);
+                for (int s = 0; s < samples_per_pixel; ++s)
+                {
+                    auto u = (i + random_double()) / (image_width - 1);
+                    auto v = (j + random_double()) / (image_height - 1);
+                    ray r = cam.get_ray(u, v);
+                    pixel_color += ray_color_10(r, world, max_depth);
+                }
+                write_color_8(std::cout, pixel_color, samples_per_pixel);
+            }
+        }
+        break;
+    }
+    case 11:
+    {
+        // Image
+
+        const auto aspect_ratio = 16.0 / 9.0;
+        const int image_width = 400;
+        const int image_height = static_cast<int>(image_width / aspect_ratio);
+        const int samples_per_pixel = 100;
+        const int max_depth = 50;
+
+        // World
+        hittable_list world;
+        auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+        auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+        auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8));
+        auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2));
+
+        world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+        world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+        world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+        world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+
+        // Camera
+        camera cam;
+
+        // Render
+
+        std::cout << "P3\n"
+                  << image_width << ' ' << image_height << "\n255\n";
+
+        for (int j = image_height - 1; j >= 0; --j)
+        {
+            std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            for (int i = 0; i < image_width; ++i)
+            {
+                color pixel_color(0, 0, 0);
+                for (int s = 0; s < samples_per_pixel; ++s)
+                {
+                    auto u = (i + random_double()) / (image_width - 1);
+                    auto v = (j + random_double()) / (image_height - 1);
+                    ray r = cam.get_ray(u, v);
+                    pixel_color += ray_color_11(r, world, max_depth);
                 }
                 write_color_8(std::cout, pixel_color, samples_per_pixel);
             }
