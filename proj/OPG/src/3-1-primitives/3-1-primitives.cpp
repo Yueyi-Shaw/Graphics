a5f2ac6e08780d23aa52b99f0e4e5c0ff85364a4
yueyi <1481796592@qq.com>
2023-05-28 23:06:55 +0800

    update 3.1

--- proj/OPG/src/3-1-primitives/3-1-primitives.cpp
+++ proj/OPG/src/3-1-primitives/3-1-primitives.cpp
@@ -21,12 +21,52 @@ public:
     {
         ApplicationTemplate::Initialize(title);
         std::cout << "init start" << std::endl;
-        static const GLfloat vertices[4][2] =
+        static const GLfloat vertices[40][2] =
             {
+                // Points
                 {-0.8, -0.8},
-                {0.8, -0.8},
                 {0.8, 0.8},
-                {-0.8, 0.8}};
+                {0.8, -0.8},
+                {-0.8, 0.8},
+                // triangle
+                {0., 0.},
+                {0.05, 0.2},
+                {0.1, 0.},
+                {0.15, 0.2},
+                {0.2, 0.},
+                {0.25, 0.2},
+                {0.3, 0.},
+                {0.35, 0.2},
+                {0.4, 0.},
+                {0.45, 0.2},
+                {0.5, 0.},
+                {0.55, 0.2},
+                // triangle strip
+                {0., 0.3},
+                {0.05, 0.5},
+                {0.1, 0.3},
+                {0.15, 0.5},
+                {0.2, 0.3},
+                {0.25, 0.5},
+                {0.3, 0.3},
+                {0.35, 0.5},
+                {0.4, 0.3},
+                {0.45, 0.5},
+                {0.5, 0.3},
+                {0.55, 0.5},
+                // triangle fan
+                {0., 0.6},
+                {0.05, 0.8},
+                {0.1, 0.6},
+                {0.15, 0.8},
+                {0.2, 0.6},
+                {0.25, 0.8},
+                {0.3, 0.6},
+                {0.35, 0.8},
+                {0.4, 0.6},
+                {0.45, 0.8},
+                {0.5, 0.6},
+                {0.55, 0.8}};
 
         glCreateVertexArrays(1, VAOs);
         glCreateBuffers(1, VBOs);
@@ -67,12 +107,27 @@ public:
             glUniform2f(u_resolutionLoc1, (GLfloat)resolution[0], (GLfloat)resolution[1]);
         }
 
-        glEnable(GL_POINT_SPRITE);
-        glEnable(GL_PROGRAM_POINT_SIZE);
-        glPointSize(64.0f);
-        glBindVertexArray(VAOs[0]);
-        glDrawArrays(GL_POINTS, 0, 4);
+        glPolygonMode(GL_FRONT, GL_LINE);
+        glPolygonMode(GL_BACK, GL_FILL);
 
+        glFrontFace(GL_CW);
+        // draw points
+        // glEnable(GL_POINT_SPRITE); // use gl_PointCoord to see effects
+        // glEnable(GL_PROGRAM_POINT_SIZE);
+        // glPointSize(64.0f);
+        // glBindVertexArray(VAOs[0]);
+        // glDrawArrays(GL_POINTS, 0, 4);
+
+        // draw lines
+        // glLineWidth(64.0f);
+        // glDrawArrays(GL_LINES, 0, 4);
+        // glDrawArrays(GL_LINE_LOOP, 0, 4);
+        // glDrawArrays(GL_LINE_STRIP, 0, 4);
+
+        // draw triangle
+        glDrawArrays(GL_TRIANGLES, 4, 12);
+        glDrawArrays(GL_TRIANGLE_STRIP, 16, 12);
+        glDrawArrays(GL_TRIANGLE_FAN, 28, 12);
         ApplicationTemplate::Display();
     }
 
--- proj/OPG/src/3-1-primitives/shader.frag
+++ proj/OPG/src/3-1-primitives/shader.frag
@@ -6,7 +6,8 @@ uniform vec2 u_resolution;
 uniform float u_time;
 
 void main(){
-    vec2 st=gl_PointCoord.xy;
+    // vec2 st=gl_PointCoord.xy;
+    vec2 st=gl_FragCoord.xy/u_resolution.xy;
     st.x*=u_resolution.x/u_resolution.y;
 
     vec3 color=vec3(0.);
