0259c06fbcf44838f74ee5a8d76c1a306d4a035b
yueyi <1481796592@qq.com>
2023-05-31 13:45:38 +0800

    update test demo

--- proj/OPG/src/0-2-assimpDemo/0-2-assimpDemo.cpp
+++ proj/OPG/src/0-2-assimpDemo/0-2-assimpDemo.cpp
@@ -1,10 +1,13 @@
 #include <assimp/Importer.hpp>  // C++ importer interface
 #include <assimp/scene.h>       // Output data structure
 #include <assimp/postprocess.h> // Post processing flags
+#include "Tools/DebugConsole.h"
 #include <iostream>
 
 int main(int argc, char **argv)
 {
+    DebugConsole console;
+
     // Create an instance of the Importer class
     Assimp::Importer importer;
 
@@ -19,12 +22,47 @@ int main(int argc, char **argv)
                                                  aiProcess_JoinIdenticalVertices |
                                                  aiProcess_SortByPType);
 
-    // If the import failed, report it
-    if (nullptr == scene)
+    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
     {
-        std::cout << importer.GetErrorString() << std::endl;
-        return false;
+        std::cerr << "Failed to load model: " << importer.GetErrorString() << std::endl;
+        return -1;
     }
 
-    // Now we can access the file's contents.
+    // log
+    std::cout << "Model has " << scene->mNumMeshes << " meshes." << std::endl;
+
+    aiNode *rootNode = scene->mRootNode;
+    std::cout << "Model root node name: " << rootNode->mName.C_Str() << std::endl;
+    std::cout << "Model root node has " << rootNode->mNumChildren << " children." << std::endl;
+
+    // log
+    for (unsigned int i = 0; i < scene->mNumMeshes; i++)
+    {
+        aiMesh *mesh = scene->mMeshes[i];
+
+        std::cout << "Mesh " << (i + 1) << " has " << mesh->mNumVertices << " vertices." << std::endl;
+
+        for (unsigned int j = 0; j < mesh->mNumVertices; j++)
+        {
+            aiVector3D position = mesh->mVertices[j];
+            std::cout << "Vertex " << (j + 1) << " position: (" << position.x << ", " << position.y << ", " << position.z << ")" << std::endl;
+        }
+    }
+
+    // release
+    importer.FreeScene();
+
+    std::cout << "PRESS ENTER TO EXIT" << std::endl;
+    std::string userInput;
+    while (true)
+    {
+        std::getline(std::cin, userInput);
+
+        if (!userInput.empty())
+        {
+            break;
+        }
+    }
+
+    return 0;
 }
